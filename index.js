f64a615e7bc5d1ff3612f4d5ad6f32c3b54f2bde
Gordon <gordonjackman@gmail.com>
2024-01-04 12:25:09 -0500

    i think i need to spread things out into more scripts.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes');
-const colors = require('./lib/colors');
+// const shapes = require('./lib/shapes');
+// const colors = require('./lib/colors');
 
 inquirer
     .prompt ([
@@ -20,8 +20,23 @@ inquirer
             type: 'input',
             message: 'Enter a color for your font',
             name: 'textColor',
-            
-
+            when: (answers) => {
+                if (answers.colorMethod === 'Color Keyword') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter a color for your font',
+            name: 'textColor',
+            when: (answers) => {
+                if (answers.colorMethod === 'Hexadecimal') {
+                    return true;
+                }
+                return false;
+            }
         },
         {
             type: 'list',
@@ -30,9 +45,45 @@ inquirer
             name: 'shape'
         },
         {
-            type: 'prompt',
-            message: 'What color would you like the shape? Enter a color or hexadecimal color code.',
-            name: 'shapeColor'
+            type: 'input',
+            message: 'What color would you like the shape?',
+            name: 'shapeColor',
+            when: (answers) => {
+                if (answers.colorMethod === 'Color Keyword') {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            message: 'What color would you like the shape?',
+            name: 'shapeColor',
+            when: (answers) => {
+                if (answers.colorMethod === 'Hexadecimal') {
+                    return true;
+                }
+                return false;
+            }
         }
     ])
+
+    // function logoGenerator(answers) {
+
+    //     if (answers.shape === 'Triangle') {
+    //         let logo = new Triangle (answers.initials, answers.textColor, answers.shapeColor)
+    //         return logo.render()
+    //     } else if (answers.shape === 'Square') {
+    //         let logo = new Square (answers.initials, answers.textColor, answers.shapeColor)
+    //         return logo.render()
+    //     } else {
+    //         let logo = new Circle (answers.initials, answers.textColor, answers.shapeColor)
+    //         return logo.render()
+    //     }
+    // }
+
+    // function saveLogo(answers) {
+    //     const customLogo = logoGenerator(answers)
+    //     fs.writeFile("./examples/logo.svg", svg, () => console.info("Generated logo.svg"));
+    // }
 
